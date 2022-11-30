e040aecfe8152ec3d13b4bd515709570c087424d
surbaniec <radsler1999@gmail.com>
2022-11-30 14:21:27 +0100

    6. Add Register page, add FormRow component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,54 @@
+import { useState, useEffect } from 'react';
+import { FormRow, Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper className='full-page'>
+      <form onSubmit={onSubmit} className='form'>
+        <Logo />
+        <h3>Login</h3>
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default Register;
