29d1bcfb0df7cc0da4c0a95b8bcb364c2d2399ac
surbaniec <radsler1999@gmail.com>
2022-12-04 12:32:29 +0100

    17. Add ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Landing, Error, Register } from './pages';
+import { Landing, Error, Register, ProtectedRoute } from './pages';
 import {
   Profile,
   AddJob,
@@ -14,7 +14,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SharedLayout />}>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path='all-jobs' element={<AllJobs />} />
           <Route path='add-job' element={<AddJob />} />
