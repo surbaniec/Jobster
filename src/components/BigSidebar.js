c7283d8cd812fc193a2f9724729793cccbe20c3a
surbaniec <radsler1999@gmail.com>
2022-12-04 14:04:41 +0100

    20. Create BigSidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,7 +1,27 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import NavLinks from './NavLinks';
 
 function BigSidebar() {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default BigSidebar;
