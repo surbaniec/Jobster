f484cb23cb386a3d9b81aa13b74e335a69f09f10
surbaniec <radsler1999@gmail.com>
2022-12-06 19:48:36 +0100

    37. Add showStats functionality

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -35,6 +35,18 @@ export const getAllJobs = createAsyncThunk(
   }
 );
 
+export const showStats = createAsyncThunk(
+  'allJobs/showStats',
+  async (_, thunkAPI) => {
+    try {
+      const res = await customFetch.get('/jobs/stats');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const allJobsSlice = createSlice({
   name: 'allJobs',
   initialState,
@@ -55,6 +67,18 @@ const allJobsSlice = createSlice({
       state.jobs = payload.jobs;
     },
     [getAllJobs.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [showStats.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [showStats.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.stats = payload.defaultStats;
+      state.monthlyApplications = payload.monthlyApplications;
+    },
+    [showStats.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { showStats } from '../../features/allJobs/allJobsSlice';
 
 const Stats = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
+
   return <div>Stats</div>;
 };
 
