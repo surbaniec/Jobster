f4ecd6740477def2c874cd6d359da987e9ab687a
surbaniec <radsler1999@gmail.com>
2022-12-07 20:36:05 +0100

    41. Add search functionality

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,6 +1,7 @@
 import { FormRow, FormRowSelect } from './';
 import Wrapper from '../assets/wrappers/SearchContainer';
 import { useSelector, useDispatch } from 'react-redux';
+import { handleChange, clearFilters } from '../features/allJobs/allJobsSlice';
 
 function SearchContainer() {
   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
@@ -9,9 +10,12 @@ function SearchContainer() {
 
   const dispatch = useDispatch();
 
-  const handleSearch = (e) => {};
+  const handleSearch = (e) => {
+    dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(clearFilters());
   };
 
   return (
